79a85d63034c57b42998dedbbfd88b705a685fe2
Scott T <scott.tessm@gmail.com>
2021-09-13 01:45:25 -0500

    Completed styling for resume page

--- portfolio/src/components/pages/Resume.js
+++ portfolio/src/components/pages/Resume.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import '../../styles/allPages.css';
 import myResume from './assets/pdf/Scott_Tessman_Resume.pdf';
+import '../../styles/resumePage.css';
 
 export default function Blog() {
   return (
-    <div className="fillPage">
-      <h1>Resume</h1>
-      
-      <h2>Download my resume <a src={myResume} download="Scott_Tessman_Resume.pdf">here</a></h2> {'change anchor to button'}
-      <h4>Front-end Proficiency</h4>
-      <ul>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+      <h4 className="resumeHeader">Front-end Proficiency</h4>
+      <ul className="resumeList">
         <li>HTML</li>
         <li>CSS</li>
         <li>Javascript</li>
@@ -17,9 +17,10 @@ export default function Blog() {
         <li>React</li>
         <li>Bootstrap</li>
       </ul>
-
-      <h4>Back-end Proficiency</h4>
-      <ul>
+      </div>
+      <div className="col">
+      <h4 className="resumeHeader">Back-end Proficiency</h4>
+      <ul className="resumeList">
         <li>APIs</li>
         <li>Node</li>
         <li>Express</li>
@@ -28,28 +29,9 @@ export default function Blog() {
         <li>REST</li>
         <li>GraphQL</li>
       </ul>
-
-      <h4>Back-end Proficiency</h4>
-      <ul>
-        <li>APIs</li>
-        <li>Node</li>
-        <li>Express</li>
-        <li>MySQL/Sequelize</li>
-        <li>MongoDB/Mongoose</li>
-        <li>REST</li>
-        <li>GraphQL</li>
-      </ul>
-
-      <h4>Back-end Proficiency</h4>
-      <ul>
-        <li>APIs</li>
-        <li>Node</li>
-        <li>Express</li>
-        <li>MySQL/Sequelize</li>
-        <li>MongoDB/Mongoose</li>
-        <li>REST</li>
-        <li>GraphQL</li>
-      </ul>
+      </div>
+      </div>
+      <a className="pdfLink" href={myResume} download="Scott_Tessman_Resume.pdf">Download Resume</a>
     </div>
   );
 }
