2be3f547edfe9daee851c450c16103dbf2d5e846
Scott T <scott.tessm@gmail.com>
2021-09-13 01:03:16 -0500

    Wrapped up styling portfolio page.

--- portfolio/src/components/PortfolioContainer.js
+++ portfolio/src/components/PortfolioContainer.js
@@ -8,7 +8,7 @@ import Footer from './Footer';
 import '../styles/allPages.css';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('About');
 
   const renderPage = () => {
     if (currentPage === 'About') {
--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -4,40 +4,64 @@ import pawsNPals from './assets/img/PawsnPals.png';
 import techBlog from './assets/img/Tech_Blog_homepage.png';
 import noteTaker from './assets/img/Note_Taker.png';
 import weatherDashboard from './assets/img/weatherDashboard.png';
+import '../../styles/portfolioPage.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <h3>IDK, You Pick</h3>
-      <img alt="idkYouPick_image" src={idkYouPick}></img>
-      <a href="https://github.com/stessman/Group-Collaboration"  target="_blank" rel="noreferrer">Github link</a>
-      <a href="https://scoven2.github.io/Group-Collaboration/"  target="_blank" rel="noreferrer">link to site</a>
+    <div className="container portContainer">
+      <div className="row">
+      <div className="col">
+      <h3 className="portHeader">IDK, You Pick</h3>
+      <a href="https://scoven2.github.io/Group-Collaboration/" target="_blank" rel="noreferrer">
+      <img className="portImg" alt="idkYouPick_image" src={idkYouPick}></img>
+      </a>
+      <a className="ghLink" href="https://github.com/stessman/Group-Collaboration"  target="_blank" rel="noreferrer">GitHub</a>
+      </div>
 
-      <h3>Paws and Pals - Pet Shelter</h3>
-      <img alt="pawsNPals_image" src={pawsNPals}></img>
-      <a href="https://github.com/stessman/Project-2"  target="_blank" rel="noreferrer">Github link</a>
-      <a href="https://sleepy-cove-71764.herokuapp.com/"  target="_blank" rel="noreferrer">link to site</a>
+      <div className="col">
+      <h3 className="portHeader">Paws and Pals - Pet Shelter</h3>
+      <a href="https://sleepy-cove-71764.herokuapp.com/" target="_blank" rel="noreferrer">
+      <img className="portImg" alt="pawsNPals_image" src={pawsNPals}></img>
+      </a>
+      <a className="ghLink" href="https://github.com/stessman/Project-2"  target="_blank" rel="noreferrer">GitHub</a>
+      </div>
+      </div>
 
-      <h3>Tech Blog</h3>
-      <img alt="techBlog_image" src={techBlog}></img>
-      <a href="https://github.com/stessman/Tech-Blog"  target="_blank" rel="noreferrer">Github link</a>
-      <a href="https://calm-crag-87567.herokuapp.com/"  target="_blank" rel="noreferrer">link to site</a>
+      <div className="row">
+      <div className="col">
+      <h3 className="portHeader">Tech Blog</h3>
+      <a href="https://calm-crag-87567.herokuapp.com/" target="_blank" rel="noreferrer">
+      <img className="portImg" alt="techBlog_image" src={techBlog}></img>
+      </a>
+      <a className="ghLink" href="https://github.com/stessman/Tech-Blog"  target="_blank" rel="noreferrer">GitHub</a>
+      </div>
 
-      <h3>Note Taker</h3>
-      <img alt="noteTaker_image" src={noteTaker}></img>
-      <a href="https://github.com/stessman/Note-Taker"  target="_blank" rel="noreferrer">Github link</a>
-      <a href="https://safe-hollows-62419.herokuapp.com/"  target="_blank" rel="noreferrer">link to site</a>
+      <div className="col">
+      <h3 className="portHeader">Note Taker</h3>
+      <a href="https://safe-hollows-62419.herokuapp.com/" target="_blank" rel="noreferrer">
+      <img className="portImg" alt="noteTaker_image" src={noteTaker}></img>
+      </a>
+      <a className="ghLink" href="https://github.com/stessman/Note-Taker"  target="_blank" rel="noreferrer">GitHub</a>
+      </div>
+      </div>
 
-      <h3>Weather Dashboard</h3>
-      <img alt="weatherDashboard_image" src={weatherDashboard}></img>
-      <a href="https://github.com/stessman/06-Weather-Dashboard"  target="_blank" rel="noreferrer">Github link</a>
-      <a href="https://stessman.github.io/06-Weather-Dashboard/"  target="_blank" rel="noreferrer">link to site</a>
+      <div  className="row">
+      <div className="col">
+      <h3 className="portHeader">Weather Dashboard</h3>
+      <a href="https://stessman.github.io/06-Weather-Dashboard/" target="_blank" rel="noreferrer">
+      <img className="portImg" alt="weatherDashboard_image" src={weatherDashboard}></img>
+      </a>
+      <a className="ghLink" href="https://github.com/stessman/06-Weather-Dashboard"  target="_blank" rel="noreferrer">GitHub</a>
+      </div>
 
-      <h3>Workout Tracker FINISH ME</h3>
-      <img alt="workoutTracker_image" src=""></img>
-      <a href="https://github.com/stessman/Workout-Tracker"  target="_blank" rel="noreferrer">Github link</a>
-      <a href="finishme"  target="_blank" rel="noreferrer">link to site</a>
+      <div className="col">
+      <h3 className="portHeader">Workout Tracker FINISH ME</h3>
+      <a href="" target="_blank" rel="noreferrer">
+      <img className="portImg" alt="workoutTracker_image" src=""></img>
+      </a>
+      <a className="ghLink" href="https://github.com/stessman/Workout-Tracker"  target="_blank" rel="noreferrer">GitHub</a>
+      </div>
+      </div>
     </div>
   );
 }
